e749b50cfd882a342beb2ec277e45e6329610bf0
David Lönnhager <dv.lnh.d@gmail.com>
2018-12-27 23:08:29 +0100

    Add more packages.

---
+++ Formula/source-highlight.rb
@@ -0,0 +1,32 @@
+class SourceHighlight < Formula
+  desc "Source-code syntax highlighter"
+  homepage "https://www.gnu.org/software/src-highlite/"
+  url "https://ftp.gnu.org/gnu/src-highlite/source-highlight-3.1.8.tar.gz"
+  mirror "https://ftpmirror.gnu.org/src-highlite/source-highlight-3.1.8.tar.gz"
+  mirror "https://fossies.org/linux/www/source-highlight-3.1.8.tar.gz"
+  sha256 "01336a7ea1d1ccc374201f7b81ffa94d0aecb33afc7d6903ebf9fbf33a55ada3"
+  revision 10
+
+  bottle do
+    root_url "https://github.com/pygame/homebrew-portmidi/releases/download/bottles0"
+    rebuild 1
+    sha256 "bee2b51657cf453765c9b3c1346f38ab6cd19d610c89b78ea811afda724a4095" => :el_capitan
+  end
+
+  depends_on "boost"
+
+  needs :cxx11
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-boost=#{Formula["boost"].opt_prefix}"
+    system "make", "install"
+
+    bash_completion.install "completion/source-highlight"
+  end
+
+  test do
+    assert_match /GNU Source-highlight #{version}/, shell_output("#{bin}/source-highlight -V")
+  end
+end
